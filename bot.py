ba171717f10b623f71b11fe93061dfea39ef7a32
korn1121 <kornx1121@gmail.com>
2025-11-02 19:54:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from threading import Thread
 app = Flask('')
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "ตื่นแล้วจ้า!"
 def run_flask():
     app.run(host='0.0.0.0', port=8080)
 def keep_alive():
